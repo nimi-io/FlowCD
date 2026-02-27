ce513f598e8a773c954504df9115ab80e082a3e2
nimi <ibukunoluwaodunsi@gmail.com>
2026-02-27 09:28:50 +0100

    Update clusters.ts

--- UI/src/lib/api/clusters.ts
+++ UI/src/lib/api/clusters.ts
@@ -1,10 +1,14 @@
 import { fetchCluster, fetchClusters } from "../mock/clusters";
 import { Cluster } from "../schemas";
+import { api } from "./client";
+import { MOCK_MODE } from "./index";
 
 export async function getClusters(): Promise<Cluster[]> {
-  return fetchClusters();
+  if (MOCK_MODE) return fetchClusters();
+  return api.get<Cluster[]>("/api/clusters");
 }
 
 export async function getCluster(id: string): Promise<Cluster> {
-  return fetchCluster(id);
+  if (MOCK_MODE) return fetchCluster(id);
+  return api.get<Cluster>(`/api/clusters/${id}`);
 }
